4e3cb4d839a718a9ce624327cb81185e6526418d
ZahuEgypt <137513614+ZahuEgypt@users.noreply.github.com>
2023-06-26 16:37:19 +0300

    Add files via upload

---
+++ ZAHWA/main.js
@@ -0,0 +1,29 @@
+// function show_hide() {
+//     x = document.getElementById("cont-1");
+//     x.innerHTML = " <br> <p>سلوك بيأذي لفرلد المجتمع و بيصعب <br>الوصول لاي حد بيقوم بعمل ضد القانون </p>";
+// }
+
+// function show_hide_two() {
+//     x = document.getElementById("cont-2");
+//     x.innerHTML = " <br><p>منظر مش حضاري بيسئ لبلدنا<br> و شكلها خصوصًا لو اثار</p>";
+// }
+
+// function show_hide_three() {
+//     x = document.getElementById("cont-3");
+//     x.innerHTML = "<br><p>سلوك بيظهر ان مجتمعنا مش نضيف<br> ولازم نصححه لكل الفئات العمرية</p> ";
+// }
+
+// function show_hide_goal_one() {
+//     x = document.getElementById("goals-1");
+//     x.innerHTML = "<br><p style='direction: rtl; text-align:center;'> التحفيز علي مشاركة المجتمعات <br> المحلية في جهود الحفاظ علي <br> الممتلكات العامة و المرافق العامة. </p> ";
+// }
+
+// function show_hide_goal_two() {
+//     x = document.getElementById("goals-2");
+//     x.innerHTML = "<br> <p style='direction: rtl; text-align:center;'> تصحيح سلوك تخريب الممتلكات <br> العامة و المرافق العامة بين <br> فئات المجتمع و ضمان استمراريت <br> السلوك الجيد.</p> ";
+// }
+
+// function show_hide_goal_three() {
+//     x = document.getElementById("goals-3");
+//     x.innerHTML = "<br> <p style='direction: rtl; text-align:center;'> التعوية بأهمية الممتلكات العامة<br> و المرافق العامة و تعزيز الوعي <br> بضرورةالحفاظ عليها و العمل علي <br> مدي الحياة للحفاظ عليها.</p> ";
+// }
